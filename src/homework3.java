8a462123180079550b661042b5cde89da5fc399f
Denis <deenis033@gmail.com>
2018-06-15 12:58:58 +0300

    2

--- src/homework3.java
+++ src/homework3.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class homework3 {
     public static void main(String[] args) {
         /*
+        Задача 24
         for (int a = 1; a <= 100; a++) {
             if (a != 4 && a != 9) {
                 System.out.println(a);
             }
         }
         */
-        double n = 1;
+        double n = 1;     //Задача 23
         int b=1;
         while (n <= 1000000 ) {
             System.out.println ("День "+b+": "+n);
