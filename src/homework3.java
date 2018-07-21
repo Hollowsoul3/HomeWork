16394b20edd8270dce7ae7ad39a6fcf41af04deb
Denis <deenis033@gmail.com>
2018-07-22 01:28:58 +0300

    2

--- src/homework3.java
+++ src/homework3.java
@@ -4,17 +4,18 @@ public class homework3 {
     public static void main(String[] args) {
         /*
         Задача 24
-        for (int a = 1; a <= 100; a++) {
-            if (a != 4 && a != 9) {
-                System.out.println(a);
+        for(int i=1;i<=100;i++) {
+            if(!Integer.toString(i).contains("4") && !Integer.toString(i).contains("9")){
+                System.out.println(i);
             }
         }
         */
+
         double n = 1;     //Задача 23
-        int b=1;
-        while (n <= 1000000 ) {
-            System.out.println ("День "+b+": "+n);
-            n=1+n*3;
+        int b = 1;
+        while (n <= 1000000) {
+            System.out.println("День " + b + ": " + n);
+            n = 1 + n * 3;
             b++;
         }
     }
